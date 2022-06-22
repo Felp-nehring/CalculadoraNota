8d105773d98f7de83d220cd84550ab38195ae2ef
Felipe Nehring <felpnehring@gmail.com>
2022-06-22 12:52:29 -0300

    a

--- bimestre.php
+++ bimestre.php
@@ -1,34 +1,99 @@
 <table class="table table-dark table-hover">
 <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">Matemática</th>
-      <th scope="col">Português</th>
-      <th scope="col">História</th>
-      <th scope="col">Geografia</th>
-      <th scope="col">Filosofia</th>
-      <th scope="col">Sociologia</th>
-      <th scope="col">Literatura</th>
-      <th scope="col">Lingua Inglesa</th>
-      <th scope="col">Biologia</th>
-      <th scope="col">Química</th>
-      <th scope="col">Física</th>
-      <th scope="col">Artes</th>
+      <th scope="col bg-light"> </th>
+      <th scope="col">1º Bimestre</th>
+      <th scope="col">2º Bimestre</th>
+      <th scope="col">3º Bimestre</th>
+      <th scope="col">4º Bimestre</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <th scope="row">1</th>
-      
+    <th scope="row">Matemática</th>
+    <th scope="row"><input type="number" name="b1" id="" min="0" max="10" step=".5" class="ms-2"></th>
+    <th scope="row"><input type="number" name="b2" id="" min="0" max="10" step=".5"></th>
+    <th scope="row"><input type="number" name="b3" id="" min="0" max="10" step=".5"></th>
+    <th scope="row"><input type="number" name="b4" id="" min="0" max="10" step=".5"></th>
+    
     </tr>
     <tr>
-      <th scope="row">2</th>
-      
+    <th scope="row">Português</th>
+    <th scope="row"><input type="number" name="b1" id="" min="0" max="10" step=".5" class="ms-2"></th>
+    <th scope="row"><input type="number" name="b2" id="" min="0" max="10" step=".5"></th>
+    <th scope="row"><input type="number" name="b3" id="" min="0" max="10" step=".5"></th>
+    <th scope="row"><input type="number" name="b4" id="" min="0" max="10" step=".5"></th>
     </tr>
     <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
+    <th scope="row">História</th>
+    <th scope="row"><input type="number" name="b1" id="" min="0" max="10" step=".5" class="ms-2"></th>
+    <th scope="row"><input type="number" name="b2" id="" min="0" max="10" step=".5"></th>
+    <th scope="row"><input type="number" name="b3" id="" min="0" max="10" step=".5"></th>
+    <th scope="row"><input type="number" name="b4" id="" min="0" max="10" step=".5"></th>
     </tr>
+    <tr>
+    <th scope="row">Geografia</th>
+    <th scope="row"><input type="number" name="b1" id="" min="0" max="10" step=".5" class="ms-2"></th>
+    <th scope="row"><input type="number" name="b2" id="" min="0" max="10" step=".5"></th>
+    <th scope="row"><input type="number" name="b3" id="" min="0" max="10" step=".5"></th>
+    <th scope="row"><input type="number" name="b4" id="" min="0" max="10" step=".5"></th>
+    </tr>
+    <tr>
+    <th scope="row">Filosofia</th>
+    <th scope="row"><input type="number" name="b1" id="" min="0" max="10" step=".5" class="ms-2"></th>
+    <th scope="row"><input type="number" name="b2" id="" min="0" max="10" step=".5"></th>
+    <th scope="row"><input type="number" name="b3" id="" min="0" max="10" step=".5"></th>
+    <th scope="row"><input type="number" name="b4" id="" min="0" max="10" step=".5"></th>
+    </tr>
+    <tr>
+    <th scope="row">Sociologia</th>
+    <th scope="row"><input type="number" name="b1" id="" min="0" max="10" step=".5" class="ms-2"></th>
+    <th scope="row"><input type="number" name="b2" id="" min="0" max="10" step=".5"></th>
+    <th scope="row"><input type="number" name="b3" id="" min="0" max="10" step=".5"></th>
+    <th scope="row"><input type="number" name="b4" id="" min="0" max="10" step=".5"></th>
+    </tr>
+    <tr>
+    <th scope="row">Literatura</th> 
+    <th scope="row"><input type="number" name="b1" id="" min="0" max="10" step=".5" class="ms-2"></th>
+    <th scope="row"><input type="number" name="b2" id="" min="0" max="10" step=".5"></th>
+    <th scope="row"><input type="number" name="b3" id="" min="0" max="10" step=".5"></th>
+    <th scope="row"><input type="number" name="b4" id="" min="0" max="10" step=".5"></th>
+    </tr>
+    <tr>
+    <th scope="row">Lingua Inglesa</th>
+    <th scope="row"><input type="number" name="b1" id="" min="0" max="10" step=".5" class="ms-2"></th>
+    <th scope="row"><input type="number" name="b2" id="" min="0" max="10" step=".5"></th>
+    <th scope="row"><input type="number" name="b3" id="" min="0" max="10" step=".5"></th>
+    <th scope="row"><input type="number" name="b4" id="" min="0" max="10" step=".5"></th>
+    </tr>
+    <tr>
+    <th scope="row">Biologia</th>
+    <th scope="row"><input type="number" name="b1" id="" min="0" max="10" step=".5" class="ms-2"></th>
+    <th scope="row"><input type="number" name="b2" id="" min="0" max="10" step=".5"></th>
+    <th scope="row"><input type="number" name="b3" id="" min="0" max="10" step=".5"></th>
+    <th scope="row"><input type="number" name="b4" id="" min="0" max="10" step=".5"></th>
+    </tr>
+    <tr>
+    <th scope="row">Química</th>
+    <th scope="row"><input type="number" name="b1" id="" min="0" max="10" step=".5" class="ms-2"></th>
+    <th scope="row"><input type="number" name="b2" id="" min="0" max="10" step=".5"></th>
+    <th scope="row"><input type="number" name="b3" id="" min="0" max="10" step=".5"></th>
+    <th scope="row"><input type="number" name="b4" id="" min="0" max="10" step=".5"></th>
+    </tr>
+    <tr>
+    <th scope="row">Física</th>
+    <th scope="row"><input type="number" name="b1" id="" min="0" max="10" step=".5" class="ms-2"></th>
+    <th scope="row"><input type="number" name="b2" id="" min="0" max="10" step=".5"></th>
+    <th scope="row"><input type="number" name="b3" id="" min="0" max="10" step=".5"></th>
+    <th scope="row"><input type="number" name="b4" id="" min="0" max="10" step=".5"></th>
+    </tr>
+    <tr>
+    <th scope="row">Artes</th>
+    <th scope="row"><input type="number" name="b1" id="" min="0" max="10" step=".5" class="ms-2"></th>
+    <th scope="row"><input type="number" name="b2" id="" min="0" max="10" step=".5"></th>
+    <th scope="row"><input type="number" name="b3" id="" min="0" max="10" step=".5"></th>
+    <th scope="row"><input type="number" name="b4" id="" min="0" max="10" step=".5"></th>
+    </tr>
+   
   </tbody>
 </table>
