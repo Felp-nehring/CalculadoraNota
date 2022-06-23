99651e34fa2db33f28e9882ae1fad10a94278f7e
Felipe Nehring <felpnehring@gmail.com>
2022-06-23 14:45:30 -0300

    Separação por bimestres

--- navbar.php
+++ navbar.php
@@ -1,22 +1,22 @@
-<nav class="navbar navbar-expand-lg bg-dark">
+<nav class="navbar navbar-expand-lg bg-dark text-center">
   <div class="container-fluid ">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active mt-3 me-3" aria-current="page" href="bimestre.php"> Bimestre</a>
+          <a class="nav-link active mt-3 me-3 text-light fw-bold " aria-current="page" href="bimestre.php"> Bimestre</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active mt-3 me-4" href="trimestre.php">Trimestre</a>
+          <a class="nav-link active mt-3 me-4 text-light fw-bold " href="trimestre.php">Trimestre</a>
         </li>
-        <a class="navbar-brand" href="#"><img src="img/logo.png" alt="logo" class="logo"></a>
+        <a class="navbar-brand" href=""><img src="img/logo.png" alt="logo" class="logo"></a>
         <li class="nav-item">
-          <a class="nav-link active mt-3 ms-2" href="semestre.php">Semestre</a>
+          <a class="nav-link active mt-3 ms-2 text-light fw-bold " href="semestre.php">Semestre</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link  active mt-3 ms-3" href="more.php">Mais Informações</a>
+          <a class="nav-link  active mt-3 ms-3 text-light fw-bold " href="more.php">Mais Informações</a>
         </li>
       </ul>
     </div>
